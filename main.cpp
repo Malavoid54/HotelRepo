96dc150eb5c8a4bf2f9a2e9893489ab2644c4ee1
PurpleLion54 <fergus.weinert@gmail.com>
2022-10-12 23:33:54 +1030

    testing

--- main.cpp
+++ main.cpp
@@ -1,22 +1,47 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <iostream>
+using namespace std;
 
 
-int main(){
-   sf::RenderWindow window(sf::VideoMode(1000,1000), "SFML works!");
-   //sf::CircleShape shape(100.f);
-   //shape.setFillColor(sf::Color::Green);
+int lobby(){
+    sf::RenderWindow window(sf::VideoMode(1000,600), "Hotel Lobby");
+   //hotellobby
    sf::Texture pTexture;
    sf::Sprite playerImage;
    if(!pTexture.loadFromFile("lobby.png"))
       std::cout<<"Couldnt load image"<<std::endl;
 
    playerImage.setTexture(pTexture);
-    int temp = 0;
-     if (temp = 0){    
-    while (window.isOpen())
-    {
+
+   int temp=0;
+      while (window.isOpen()){
+       sf::Event event;
+       while (window.pollEvent(event))
+       {
+           if (event.type == sf::Event::Closed)
+               window.close();
+       }
+      //if(temp=0){
+       window.clear();
+       window.draw(playerImage);
+       window.display();
+      }
+   return 0;
+}
+
+int room(){
+   sf::RenderWindow window(sf::VideoMode(1000,600), "Hotel Room");
+
+   //hotelroom
+   sf::Texture pTexture_one;
+   sf::Sprite hotelRoom;
+   if(!pTexture_one.loadFromFile("room.jpeg"))
+      std::cout<<"Couldnt load image"<<std::endl;
+   hotelRoom.setTexture(pTexture_one);
+   
+   int temp=0;
+      while (window.isOpen()){
        sf::Event event;
        while (window.pollEvent(event))
        {
@@ -24,13 +49,54 @@ int main(){
                window.close();
        }
 
+      
        window.clear();
-       //window.draw(shape);
-       window.draw(playerImage);
+       window.draw(hotelRoom);
        window.display();
-    }
-    }
- 
-   
+      
+      }
    return 0;
+}
+
+int bar(){
+   sf::RenderWindow window(sf::VideoMode(1000,600), "Bar");
+
+   //hotelroom
+   sf::Texture pTexture_one;
+   sf::Sprite hotelBar;
+   if(!pTexture_one.loadFromFile("hotelbar.jpg"))
+      std::cout<<"Couldnt load image"<<std::endl;
+   hotelBar.setTexture(pTexture_one);
+   
+      while (window.isOpen()){
+       sf::Event event;
+       while (window.pollEvent(event))
+       {
+           if (event.type == sf::Event::Closed)
+               window.close();
+       }
+
+      
+       window.clear();
+       window.draw(hotelBar);
+       window.display();
+      
+      }
+   return 0;
+}
+
+int main(){
+   int temp = 0;
+
+   cout << "Enter the number of the room you wish to go to: Lobby - 1,  Your Room - 2, Bar - 3" << endl;
+   cout << temp<< endl;
+   cin >> temp;
+   cout << temp <<endl;
+   if (temp==1){
+   lobby();
+   } else if (temp==2){
+      room();
+   } else if (temp==3){
+      bar();
+   }
 }
